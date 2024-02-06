e941a3d0aa00bbe74a1a0e8851cfee3db3195796
Sumit <sumit@clearquote.io>
2024-02-06 18:12:01 +0530

    added matplotlib

--- utils_yolo/plots.py
+++ utils_yolo/plots.py
@@ -8,8 +8,8 @@ from copy import copy
 from pathlib import Path
 
 import cv2
-#import matplotlib
-#import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 #import pandas as pd
 #import seaborn as sns
